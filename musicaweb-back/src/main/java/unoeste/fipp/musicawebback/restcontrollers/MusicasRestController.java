5bf143b24111d8466de26477a26e5e79fa825c7d
luigi12014@hotmail.com <luigi12014@hotmail.com>
2026-09-18 05:54:03 -0300

    finalizando topicos e testando o post

--- musicaweb-back/src/main/java/unoeste/fipp/musicawebback/restcontrollers/MusicasRestController.java
+++ musicaweb-back/src/main/java/unoeste/fipp/musicawebback/restcontrollers/MusicasRestController.java
@@ -47,4 +47,54 @@ public class MusicasRestController {
         return getMusica(titulo);
     }
 
+    @GetMapping (value = "list-genre/{genero}")
+    public ResponseEntity<Object> getMusicaGenre(@PathVariable String genero){
+        List<Musica> musicas = musicasRepositorio.getMusicaGenero(genero);
+        if (musicas.isEmpty())
+            return ResponseEntity.badRequest().body(new Erro("Nenhuma musica encontrada nesse genero!"));
+        return ResponseEntity.ok(musicas);
+    }
+
+    @GetMapping(value = "list-year/{dtInicio}/{dtFim}")
+    public ResponseEntity<Object> getMusicaYear(@PathVariable int dtInicio, @PathVariable int dtFim){
+        List<Musica> musicas = musicasRepositorio.getAno(dtInicio, dtFim);
+        if(musicas.isEmpty())
+            return ResponseEntity.badRequest().body(new Erro ("Nnehuma musica neste periodo!"));
+        return ResponseEntity.ok(musicas);
+    }
+
+    @GetMapping (value = "get-generos")
+    public ResponseEntity<Object> getGeneros(){
+        return ResponseEntity.ok(musicasRepositorio.getGeneroList());
+    }
+
+    @GetMapping (value = "list-keyword/{keyword}")
+    public ResponseEntity <Object> getMusicaKeyword(@PathVariable String keyword){
+        List<Musica> musicas = musicasRepositorio.getMusicaKeyword(keyword);
+        return ResponseEntity.ok(musicas);
+    }
+
+    @PostMapping(value = "add-musica")
+    public ResponseEntity<Object> addMusica(@RequestBody Musica novaMusica){
+        if (novaMusica == null)
+            return ResponseEntity.badRequest().body(new Erro("Problemas ao adicionar a música"));
+        if (novaMusica.getTitulo() == null || novaMusica.getTitulo().trim().isEmpty())
+            return ResponseEntity.badRequest().body(new Erro("O título é obrigatório"));
+        if (novaMusica.getArtista() == null || novaMusica.getArtista().trim().isEmpty())
+            return ResponseEntity.badRequest().body(new Erro("O artista é obrigatório"));
+        if (novaMusica.getGenero() == null || novaMusica.getGenero().trim().isEmpty())
+            return ResponseEntity.badRequest().body(new Erro("O gênero é obrigatório"));
+        if (!musicasRepositorio.generoValido(novaMusica.getGenero()))
+            return ResponseEntity.badRequest().body(new Erro("Gênero inválido"));
+        if (novaMusica.getAno() == null || novaMusica.getAno().trim().isEmpty())
+            return ResponseEntity.badRequest().body(new Erro("O ano é obrigatório"));
+        try{
+            Integer.parseInt(novaMusica.getAno());
+        }catch (NumberFormatException e){
+            return ResponseEntity.badRequest().body(new Erro("O ano deve ser numérico!"));
+        }
+        musicasRepositorio.addMusica(novaMusica);
+        return ResponseEntity.ok(novaMusica);
+    }
+
 }
